84e7b05bc0776e86a3b1844f7ca284825cfb6183
LCmry <lcrees.2014@gmail.com>
2018-03-28 15:50:04 -0700

    Show subscription info in initial order recipient email

--- templates/emails/recipient-new-initial-order.php
+++ templates/emails/recipient-new-initial-order.php
@@ -33,38 +33,42 @@ if ( 'true' == $new_recipient ) : ?>
 	'</a>'
 ); ?></p>
 
-<?php endif;
+<?php endif; ?>
 
-if ( 0 < count( $subscriptions ) ) : ?>
-	<table cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;">
-<?php endif;
-
-foreach ( $subscriptions as $subscription_id ) {
-	$subscription = wcs_get_subscription( $subscription_id );?>
+<?php if ( ! empty( $subscriptions ) ) : ?>
+<h2><?php esc_html_e( 'Subscription Information:', 'woocommerce-subscriptions-gifting' ); ?></h2>
+<table class="td" cellspacing="0" cellpadding="6" style="width: 100%; font-family: 'Helvetica Neue', Helvetica, Roboto, Arial, sans-serif;" border="1">
 	<thead>
 		<tr>
-			<td style="padding: -6" colspan="3"><h2><?php printf( esc_html__( 'Subscription #%s', 'woocommerce-subscriptions-gifting' ), esc_attr( $subscription->get_order_number() ) ) ?></h2></td>
+			<th class="td" scope="col" style="text-align:left;"><?php esc_html_e( 'Subscription', 'woocommerce-subscriptions-gifting' ); ?></th>
+			<th class="td" scope="col" style="text-align:left;"><?php echo esc_html_x( 'Start Date', 'table heading',  'woocommerce-subscriptions-gifting' ); ?></th>
+			<th class="td" scope="col" style="text-align:left;"><?php echo esc_html_x( 'End Date', 'table heading',  'woocommerce-subscriptions-gifting' ); ?></th>
+			<th class="td" scope="col" style="text-align:left;"><?php echo esc_html_x( 'Period',  'table heading', 'woocommerce-subscriptions-gifting' ); ?></th>
 		</tr>
 	</thead>
-		<tr>
-			<th class="td" scope="col" style="text-align:left;"><?php esc_html_e( 'Product', 'woocommerce-subscriptions-gifting' ); ?></th>
-			<th class="td" scope="col" style="text-align:left;"><?php esc_html_e( 'Quantity', 'woocommerce-subscriptions-gifting' ); ?></th>
-		</tr>
 	<tbody>
-		<?php
-		echo wp_kses_post( WC_Subscriptions_Email::email_order_items_table( $subscription, array(
-			'show_download_links' => true,
-			'show_sku'            => false,
-			'show_purchase_note'  => true,
-		) ) );
-
-		if ( is_callable( array( 'WC_Subscriptions_Email', 'order_download_details' ) ) ) { ?>
+	<?php foreach ( $subscriptions as $subscription_id ) : ?>
+		<?php $subscription = wcs_get_subscription( $subscription_id ); ?>
 		<tr>
-			<td style="padding: 0" colspan="3"><p><?php WC_Subscriptions_Email::order_download_details( $subscription, $sent_to_admin, $plain_text, $email ); ?></p></td>
+			<td class="td" scope="row" style="text-align:left;"><a href="<?php echo esc_url( $subscription->get_view_order_url() ); ?>"><?php echo sprintf( esc_html_x( '#%s', 'subscription number in email table. (eg: #106)', 'woocommerce-subscriptions-gifting' ), esc_html( $subscription->get_order_number() ) ); ?></a></td>
+			<td class="td" scope="row" style="text-align:left;"><?php echo esc_html( date_i18n( wc_date_format(), $subscription->get_time( 'date_created', 'site' ) ) ); ?></td>
+			<td class="td" scope="row" style="text-align:left;"><?php echo esc_html( ( 0 < $subscription->get_time( 'end' ) ) ? date_i18n( wc_date_format(), $subscription->get_time( 'end', 'site' ) ) : _x( 'When Cancelled', 'Used as end date for an indefinite subscription', 'woocommerce-subscriptions-gifting' ) ); ?></td>
+			<td class="td" scope="row" style="text-align:left;">
+				<?php
+				$subscription_details = array(
+					'recurring_amount'            => '',
+					'subscription_period'         => $subscription->get_billing_period(),
+					'subscription_interval'       => $subscription->get_billing_interval(),
+					'initial_amount'              => '',
+					'use_per_slash'               => false,
+				);
+				$subscription_details = apply_filters( 'woocommerce_subscription_price_string_details', $subscription_details, $subscription );
+				echo wp_kses_post( wcs_price_string( $subscription_details ) );?>
+			</td>
 		</tr>
-		<?php } ?>
-	</tbody>
-	<?php
-}
-echo '</table>';
-do_action( 'woocommerce_email_footer', $email );
+	<?php endforeach; ?>
+</tbody>
+</table>
+<?php endif; ?>
+
+<?php do_action( 'woocommerce_email_footer', $email ); ?>
