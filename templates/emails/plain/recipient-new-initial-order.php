b68ea4dff8731d8681f5839f5858c94b9c1bbbe0
James Allan <james.allan@live.com>
2018-03-29 19:56:32 +1000

    Update the new initial order template to use new refactored templates

--- templates/emails/plain/recipient-new-initial-order.php
+++ templates/emails/plain/recipient-new-initial-order.php
@@ -25,27 +25,11 @@ if ( 'true' == $new_recipient ) {
 foreach ( $subscriptions as $subscription_id ) {
 	$subscription = wcs_get_subscription( $subscription_id );
 
-	echo sprintf( __( 'Subscription #%s', 'woocommerce-subscriptions-gifting' ), $subscription->get_order_number() ) . "\n";
-
-	echo sprintf( __( 'Start Date: %s', 'woocommerce-subscriptions-gifting' ), date_i18n( wc_date_format(), $subscription->get_time( 'date_created', 'site' ) ) ) . "\n";
-
-	echo sprintf( __( 'End Date: %s', 'woocommerce-subscriptions-gifting' ), ( 0 < $subscription->get_time( 'end' ) ) ? date_i18n( wc_date_format(), $subscription->get_time( 'end', 'site' ) ) : _x( 'When Cancelled', 'Used as end date for an indefinite subscription', 'woocommerce-subscriptions-gifting' ) ) . "\n";
-
-	$subscription_details = array(
-		'recurring_amount'            => '',
-		'subscription_period'         => $subscription->get_billing_period(),
-		'subscription_interval'       => $subscription->get_billing_interval(),
-		'initial_amount'              => '',
-		'use_per_slash'               => false,
-	);
-	$subscription_details = apply_filters( 'woocommerce_subscription_price_string_details', $subscription_details, $subscription );
-	echo sprintf( __( 'Period: %s', 'woocommerce-subscriptions-gifting' ), wp_kses_post( wcs_price_string( $subscription_details ) ) ) . "\n";
+	do_action( 'wcs_gifting_email_order_details', $subscription, $sent_to_admin, $plain_text, $email );
 
 	if ( is_callable( array( 'WC_Subscriptions_Email', 'order_download_details' ) ) ) {
 		WC_Subscriptions_Email::order_download_details( $subscription, $sent_to_admin, $plain_text, $email );
 	}
-
-	echo "----------\n\n";
 }
 
 echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
